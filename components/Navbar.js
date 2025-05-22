de1c44626164ced2af0a8f9ce4dac671c02af872
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-22 15:22:41 +0500

    ue

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,14 @@ import { ArrowRight } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav class="navbar bg-base-100 shadow-sm spacing">
-      <div class="flex-1">
-        <Link href="/" class="btn btn-ghost text-xl">
+    <nav className="navbar bg-base-100 shadow-sm spacing">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-xl">
           ThandaBox
         </Link>
       </div>
-      <div class="flex-none">
-        <ul class="menu menu-horizontal px-1">
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
           <li>
             <Link className="btn btn-accent" href="/checkout">
               Order Now <ArrowRight size={15} />
